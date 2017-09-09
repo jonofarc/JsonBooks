90559e9cb03f69e6ff8371ce27431645404e9845
Jonathan Maldonado <jmaldonadom1989@gmail.com>
2017-09-09 15:51:28 -0400

    Read booklist from http://de-coding-test.s3.amazonaws.com/books.json and displat alll item titles

--- app/src/main/java/com/example/jonathanmaldonado/bookslist/MainActivity.java
+++ app/src/main/java/com/example/jonathanmaldonado/bookslist/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.jonathanmaldonado.bookslist;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,19 +11,33 @@ import com.example.jonathanmaldonado.bookslist.Books.Book;
 import com.example.jonathanmaldonado.bookslist.Frutas.Fruta;
 import com.example.jonathanmaldonado.bookslist.Frutas.Fruta_;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG =MainActivity.class.getSimpleName()+"._TAG";
+    private static final String BASE_URL ="http://de-coding-test.s3.amazonaws.com/books.json";
     TextView resultsTV;
+    OkHttpClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         resultsTV= (TextView) findViewById(R.id.tv_results);
+
+        client = new OkHttpClient.Builder().build();
 
 
        // getFrutas();
@@ -42,31 +58,77 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "getFrutas: "+ frutas.getFrutas().get(0).getNombreFruta().toString());
         resultsTV.setText(frutas.getFrutas().get(0).getNombreFruta().toString());
     }
-    public void getBooks(){
 
-        String myjson = "[\n" +
-                "  {\n" +
-                "    \"title\" : \"Harry Potter: Complete 8-Film Collection (DVD, 2011, 8-Disc Set)\",\n" +
-                "    \"imageURL\" : \"http://i.ebayimg.com/00/$(KGrHqV,!g0E6ZCwQ)wpBOuWbUNB,g~~_6.JPG?set_id=89040003C1\"\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"title\" : \"Harry Potter and the Sorcerer's Stone (DVD, 2002, 2-Disc Set, Widescreen)\",\n" +
-                "    \"imageURL\" : \"http://i.ebayimg.com/10/!!eBlRg!EGM~$(KGrHqQOKkIE0YRojg,kBNQ)(j8Q1!~~_6.JPG?set_id=89040003C1\"\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"title\" : \"Harry Potter: Complete 8-Film Collection (Blu-ray Disc, 2011, 8-Disc Set)\",\n" +
-                "    \"imageURL\" : \"http://i.ebayimg.com/00/$(KGrHqR,!igE6M5wILm(BOuWZc0bjg~~_6.JPG?set_id=89040003C1\"\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"title\" : \"Harry Potter Years 1-7 by J. K. Rowling and Inc. Staff Scholastic (2007, Hardcover)\",\n" +
-                "    \"author\" : \"J.K. Rowling\",\n" +
-                "    \"imageURL\" : \"http://i.ebayimg.com/00/$T2eC16F,!)kE9s4Z-Ue7BRb4ZE0oog~~_6.JPG?set_id=89040003C1\"\n" +
-                "  }\n" +
-                "]";
-        Gson gson = new Gson();
-        Book[] book =  gson.fromJson(myjson, Book[].class);
-        //Log.d(TAG, "getFrutas: "+ frutas.getFrutas().get(0).getNombreFruta().toString());
-        resultsTV.setText(book[0].getTitle().toString());
+
+
+    public void getBooks() {
+
+
+        Request request = new Request.Builder().url(BASE_URL).build();
+
+        // thys is a Synchoronous request
+        // this needs a separate thread
+        // Response response = client.newCall(request).execute();
+
+        client.newCall(request).enqueue(
+                new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+
+                        if(response.isSuccessful()){
+
+
+
+                            String resp= response.body().string();
+                            try {
+
+
+                                Gson gson = new Gson();
+                                final Book[] book =  gson.fromJson(resp, Book[].class);
+
+                                final StringBuilder resultsString = new StringBuilder();
+                                for(int i = 0 ; i<book.length; i++){
+                                    resultsString.append(book[i].getTitle().toString()+"\n");
+                                }
+
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+
+                                        resultsTV.setText(resultsString.toString());
+                                    }
+                                });
+
+
+                            }catch (JsonParseException e){
+                                e.printStackTrace();
+                            }
+
+
+
+                            Log.d(TAG, "onResponse resp:  "+ resp);
+                        }else{
+                            Log.d(TAG, "onResponse: Application Error");
+                        }
+
+
+
+
+
+                    }
+                }
+        );
+
+
 
     }
+
+
+
 }
