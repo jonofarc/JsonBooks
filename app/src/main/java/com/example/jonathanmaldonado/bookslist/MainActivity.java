c52c1ba9e0e42687d99c4cc8ec633f43823e8890
Jonathan Maldonado <jmaldonadom1989@gmail.com>
2017-09-09 01:05:43 -0400

    Read JSon from http://de-coding-test.s3.amazonaws.com/books.json and display the first item title

--- app/src/main/java/com/example/jonathanmaldonado/bookslist/MainActivity.java
+++ app/src/main/java/com/example/jonathanmaldonado/bookslist/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.example.jonathanmaldonado.bookslist.Books.Book;
 import com.example.jonathanmaldonado.bookslist.Frutas.Fruta;
 import com.example.jonathanmaldonado.bookslist.Frutas.Fruta_;
 import com.google.gson.Gson;
@@ -23,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
         resultsTV= (TextView) findViewById(R.id.tv_results);
 
 
-        getFrutas();
+       // getFrutas();
+        getBooks();
 
     }
 
@@ -39,5 +41,32 @@ public class MainActivity extends AppCompatActivity {
         Fruta frutas =  gson.fromJson(myjson, Fruta.class);
         Log.d(TAG, "getFrutas: "+ frutas.getFrutas().get(0).getNombreFruta().toString());
         resultsTV.setText(frutas.getFrutas().get(0).getNombreFruta().toString());
+    }
+    public void getBooks(){
+
+        String myjson = "[\n" +
+                "  {\n" +
+                "    \"title\" : \"Harry Potter: Complete 8-Film Collection (DVD, 2011, 8-Disc Set)\",\n" +
+                "    \"imageURL\" : \"http://i.ebayimg.com/00/$(KGrHqV,!g0E6ZCwQ)wpBOuWbUNB,g~~_6.JPG?set_id=89040003C1\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"title\" : \"Harry Potter and the Sorcerer's Stone (DVD, 2002, 2-Disc Set, Widescreen)\",\n" +
+                "    \"imageURL\" : \"http://i.ebayimg.com/10/!!eBlRg!EGM~$(KGrHqQOKkIE0YRojg,kBNQ)(j8Q1!~~_6.JPG?set_id=89040003C1\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"title\" : \"Harry Potter: Complete 8-Film Collection (Blu-ray Disc, 2011, 8-Disc Set)\",\n" +
+                "    \"imageURL\" : \"http://i.ebayimg.com/00/$(KGrHqR,!igE6M5wILm(BOuWZc0bjg~~_6.JPG?set_id=89040003C1\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"title\" : \"Harry Potter Years 1-7 by J. K. Rowling and Inc. Staff Scholastic (2007, Hardcover)\",\n" +
+                "    \"author\" : \"J.K. Rowling\",\n" +
+                "    \"imageURL\" : \"http://i.ebayimg.com/00/$T2eC16F,!)kE9s4Z-Ue7BRb4ZE0oog~~_6.JPG?set_id=89040003C1\"\n" +
+                "  }\n" +
+                "]";
+        Gson gson = new Gson();
+        Book[] book =  gson.fromJson(myjson, Book[].class);
+        //Log.d(TAG, "getFrutas: "+ frutas.getFrutas().get(0).getNombreFruta().toString());
+        resultsTV.setText(book[0].getTitle().toString());
+
     }
 }
